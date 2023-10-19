cdd2577500f08471f35f34f3ff0af3d8f80cfb0d
crougheinman <gregorypeteryanoc.gpy@gmail.com>
2023-10-19 14:51:24 +0800

    component exploration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { ButtonComponent } from './component/button/button.component';
+import { TasksComponent } from './component/tasks/tasks.component';
+import { TaskItemComponent } from './component/task-item/task-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent,
+    TasksComponent,
+    TaskItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/component/tasks/tasks.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {TASKS} from "../../mock-task";
+import {Task} from "../../Task";
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.scss']
+})
+export class TasksComponent implements OnInit{
+  tasks: Task [] = TASKS;
+  
+  ngOnInit(): void {
+    
+  }
+}
