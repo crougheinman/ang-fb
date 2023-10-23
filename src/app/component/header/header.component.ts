d260c55ae0e68683f21665bc508be8f866b60708
crougheinman <gregorypeteryanoc.gpy@gmail.com>
2023-10-23 17:38:44 +0800

    toggling form

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/service/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -6,7 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
   title: string = 'Task Tracker';
-  
+  showAddTask: boolean | any;
+  subscription: Subscription | any;
+
+  constructor(private uiService : UiService) {
+    this.subscription = this.uiService.onToggle().subscribe(
+      (value) => (this.showAddTask = value)
+    );
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  toggleAddTask(){
+    this.uiService.toggleAddTask();
+  }
+
 }
