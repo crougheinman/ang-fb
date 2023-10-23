96952c86d4ad2ea05b79ce298310f1b492911a13
crougheinman <gregorypeteryanoc.gpy@gmail.com>
2023-10-23 13:26:46 +0800

    http client and deleting task

--- src/app/component/tasks/tasks.component.ts
+++ src/app/component/tasks/tasks.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {TASKS} from "../../mock-task";
+import {TaskService} from '../../service/task.service'
 import {Task} from "../../Task";
 
 @Component({
@@ -8,11 +8,21 @@ import {Task} from "../../Task";
   styleUrls: ['./tasks.component.scss']
 })
 export class TasksComponent implements OnInit{
-  tasks: Task [] = TASKS;
+  tasks: Task [] = [];
 
-  constructor() {}
+  constructor(private taskService: TaskService) {}
   
   ngOnInit(): void {
+    this.taskService.getTasks().subscribe(
+      (tasks) => this.tasks = tasks
+    );
+  }
 
+  deleteTask(task : Task) {
+    this.taskService
+      .deleteTask(task)
+      .subscribe(
+        () => this.tasks = this.tasks.filter(t => t.id !== task.id)
+      );
   }
 }
